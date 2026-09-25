3000e510401ba2a453e6f37b553b157b2ebfd369
Mathieu Grandjean <mathieu.grandjean@etu.imt-nord-europe.fr>
2026-09-25 11:06:08 +0200

    feat: add fibonacci function signature

--- notebook.py
+++ notebook.py
@@ -4,7 +4,17 @@ __generated_with = "0.24.2"
 app = marimo.App()
 
 
+@app.cell
+def _():
+    import pytest
+    import marimo as mo
 
+    return
+
+
+@app.function
+def fibonacci(n: int) -> int:
+    ...
 
 
 if __name__ == "__main__":
