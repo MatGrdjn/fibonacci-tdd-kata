e770d93e1574647339f61abd9dc2c45bf1b3ab17
Mathieu Grandjean <mathieu.grandjean@etu.imt-nord-europe.fr>
2026-09-25 11:13:01 +0200

    test: add unit tests for fibonacci

--- notebook.py
+++ notebook.py
@@ -9,12 +9,47 @@ def _():
     import pytest
     import marimo as mo
 
-    return
+    return (pytest,)
 
 
 @app.function
 def fibonacci(n: int) -> int:
     ...
+
+
+@app.cell
+def _(pytest):
+    @pytest.mark.parametrize(
+        ("n", "expected"),
+        [
+            (0, 0),
+            (1, 1),
+            (2, 1),
+            (5, 5)
+        ],
+    )
+    def test_fibonacci_suscess(n, expected):
+        assert fibonacci(n) == expected
+
+    return
+
+
+@app.cell
+def _(pytest):
+    @pytest.mark.parametrize(
+        ("input", "expected_exception", "expected_message"),
+        [
+            (-1, ValueError, "n must be positive or null"),
+            ("1", TypeError, "n must be an int"),
+            (1.5, TypeError, "n must be an int"),
+            (True, TypeError, "n must be an int"),
+        ],
+    )
+    def test_fibonacci_errors(input, expected_exception, expected_message):
+        with pytest.raises(expected_exception, match=expected_message):
+            fibonacci(input)
+
+    return
 
 
 if __name__ == "__main__":
