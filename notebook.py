97a5a9961d811782365a5021c9707ace20b95006
Mathieu Grandjean <mathieu.grandjean@etu.imt-nord-europe.fr>
2026-09-25 11:15:01 +0200

    feat: implement fibonacci function

--- notebook.py
+++ notebook.py
@@ -14,7 +14,18 @@ def _():
 
 @app.function
 def fibonacci(n: int) -> int:
-    ...
+
+    if not isinstance(n, int) or isinstance(n, bool):
+        raise TypeError("n must be an int")
+    if n < 0:
+        raise ValueError("n must be positive or null")
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return fibonacci(n-1) + fibonacci(n-2)
 
 
 @app.cell
