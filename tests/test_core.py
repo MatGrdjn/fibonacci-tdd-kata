ccfd64b10d78d46a7e924f150d8c3259f612f6cf
Mathieu Grandjean <mathieu.grandjean@etu.imt-nord-europe.fr>
2026-09-25 12:15:56 +0200

    test: add the errors tests to the test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -13,3 +13,16 @@ from fibonacci_kata import fibonacci
 )
 def test_fibonacci_suscess(n, expected):
     assert fibonacci(n) == expected
+
+@pytest.mark.parametrize(
+    ("input", "expected_exception", "expected_message"),
+    [
+        (-1, ValueError, "n must be positive or null"),
+        ("1", TypeError, "n must be an int"),
+        (1.5, TypeError, "n must be an int"),
+        (True, TypeError, "n must be an int"),
+    ],
+)
+def test_fibonacci_errors(input, expected_exception, expected_message):
+    with pytest.raises(expected_exception, match=expected_message):
+        fibonacci(input)
